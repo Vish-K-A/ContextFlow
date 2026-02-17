f586433e0f2db37427acb2b167624ea4a4d42cf0
Vishruta <pes1ug24cs538@pesuonline.onmicrosoft.com>
2026-02-17 17:31:24 +0530

    Feat:Updated  chatuploadview

--- documents/views.py
+++ documents/views.py
@@ -1,8 +1,11 @@
+import os
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import FileFieldForm
 from .models import Message, Attachment
 from django.views.generic.edit import FormView
+from django.http import JsonResponse
+from .utils import setup_vectorstore, get_advanced_chain
 
 class ChatUploadView(FormView):
     form_class = FileFieldForm
@@ -18,14 +21,51 @@ class ChatUploadView(FormView):
             user=self.request.user if self.request.user.is_authenticated else None
         )
 
+        attachment_paths = []
+
         for f in files:
-            Attachment.objects.create(
+            attachment = Attachment.objects.create(
                 message=message_instance,
                 user=self.request.user if self.request.user.is_authenticated else None,
                 file=f
             )
+            attachment_paths.append(attachment.file.path)
+
+        if prompt and attachment_paths:
+            try:
+                pdf_paths = [path for path in attachment_paths if path.lower().endswith('.pdf')]
+
+                if pdf_paths:
+                    retriever = setup_vectorstore(pdf_paths[0])
+                    chain = get_advanced_chain(retriever)
+
+                    response = chain({
+                        "question": prompt,
+                        "chat_history":[]
+                    })
+
+                    ai_message = Message.objects.create(
+                        text=response['answer'],
+                        user=None
+                    )
+                    
+                    self.request.session['last_retriever'] = pdf_paths[0]
+                    self.request.session['chat_history'] = [
+                        (prompt, response['answer'])
+                    ]
+            
+            except Exception as e:
+                print(f"RAG processing error: {e}")
+
 
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = Message.objects.filter(
+            user=self.request.user if self.request.user.is_authenticated else None
+        )[:25]
+        return context
 
 @login_required    
 def user_file_views(request):
@@ -35,3 +75,4 @@ def user_file_views(request):
         'total_files': user_files.count()
     }
     return render(request, 'documents/files.html', context)
+
